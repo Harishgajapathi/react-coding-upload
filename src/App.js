92fc1ed211e9a9b0eee70be0743b98f2556c192b
Harishgajapathi <harishgajapathi99@gmail.com>
2023-05-20 11:12:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="Main-container">
+
+  <div class="diamond-shape">
+    <div className="content-container">
+      <h1 className="heading1">HTML<span className="spanEl">&</span>CSS</h1>
+      <p className="paraEl">design and build websites</p>
+    </div>
+  </div>
+</div>
+    </>
+  );
+}
+
+export default App;
